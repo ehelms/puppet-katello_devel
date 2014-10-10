97440b9a956d94d206567c9a24f5ca1d93f345ea
Eric D. Helms <ericdhelms@gmail.com>
2014-10-10 08:57:13 -0400

    Git updates.

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,14 +9,28 @@ class katello_devel::install {
     ensure    => present,
     provider  => git,
     source    => 'https://github.com/theforeman/foreman.git',
-    user      => $katello_devel::user
+    user      => $katello_devel::user,
+    remote    => "upsream",
   }
 
   vcsrepo { "${katello_devel::deployment_dir}/katello":
     ensure    => present,
     provider  => git,
     source    => 'https://github.com/Katello/katello.git',
-    user      => $katello_devel::user
+    user      => $katello_devel::user,
+    remote    => "upsream",
+  }
+
+  exec { "katello_prs":
+    command => "git config --add remote.origin.fetch '+refs/pull/*/head:refs/remotes/origin/pr/'",
+    cwd     => "${katello_devel::deployment_dir}/foreman",
+    user      => $katello_devel::user,
+  }
+
+  exec { "foreman_prs":
+    command => "git config --add remote.origin.fetch '+refs/pull/*/head:refs/remotes/origin/pr/'",
+    cwd     => "${katello_devel::deployment_dir}/foreman"
+    user    => $katello_devel::user,
   }
 
 }
